0761b88493c8ea0630dbba643ee9edf125ed8255
edy1013 <edisan.ibisi@gmail.com>
2021-09-08 13:17:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+       <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
